5a8cac921b74f899aa9409c7d78e3e099ce0002a
Silas Neto <silas-c@users.noreply.github.com>
2026-09-22 15:13:28 -0400

    lambda stuff

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,11 +18,13 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
-        val callback : (Int) -> Unit = {}
+        val callback : (Int) -> Unit = { number ->
+            displayTextView.textSize = number.toFloat()
+        }
 
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers, callback)
 
 
     }
